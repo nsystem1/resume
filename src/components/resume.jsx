e301d15b5791a75c54da2f51b17d8dd8ea59989f
Udo <writeudo@gmail.com>
2018-01-11 08:08:00 +0100

    initial config done

---
+++ src/components/resume.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Resume = () => {
+	return <div className="default-theme">This is my resume changed</div>;
+};
+
+export default Resume;
