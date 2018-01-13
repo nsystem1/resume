96e52ddc3295949c929cf09acab521072ee75c10
Udo <writeudo@gmail.com>
2018-01-13 09:19:15 +0100

    make sheet responsive

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -34,7 +34,7 @@ const Resume = () => {
 							&nbsp;/udoka
 						</a>
 					</span>
-					<span className="flex-item right-align">
+					<span className="flex-item">
 						<a className="contact-item" href="https://ng.linkedin.com/in/thisisudo">
 							<i className="mdi mdi-linkedin-box" />
 							&nbsp;/thisisudo
@@ -46,6 +46,37 @@ const Resume = () => {
 						Passionate about learning and building stuff for the web. Able to understand business requirements, prioritise work based on highest business value and work with teams to solve
 						problems.
 					</p>
+				</section>
+				<section className="section">
+					<h2>Experience</h2>
+					<div className="flex">
+						<article className="flex-item">
+							<h3>
+								Front-End Engineer Facebook <small className="date">2018-2020</small>
+							</h3>
+							<p>
+								I was a part of the team that used the MEAN stack (MongoDB, Express.js, AngularJS, and Node.js) to develop an internal application to help Andela track and manage ed support
+								Andela's Now We Give initiative, a program that offers to build websites for clients in exchange for charitable donations.
+							</p>
+							<div className="tags">
+								<span className="tag-item">css</span>
+								<span className="tag-item">html</span>
+							</div>
+						</article>
+						<article className="flex-item">
+							<h3>
+								Front-End Engineer Facebook <small className="date">2018-2020</small>
+							</h3>
+							<p>
+								I was a part of the team that used the MEAN stack (MongoDB, Express.js, AngularJS, and Node.js) to develop an internal applicatioor clients in exchange for charitable
+								donations.
+							</p>
+							<div className="tags">
+								<span className="tag-item">css</span>
+								<span className="tag-item">html</span>
+							</div>
+						</article>
+					</div>
 				</section>
 				<section className="section">
 					<h2>Experience</h2>
