7c3fc1687754f57c9c8f26c6035819dabc5ba648
Udo <writeudo@gmail.com>
2018-01-14 13:10:06 +0100

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,10 @@
-npm run pdf && git add . && git commit -m "new build" && git push origin master &&
+npm run build && npm run pdf &&
 
-git checkout gh-pages &&
+git add . && git commit -m "new build" &&
+
+git push origin master && git checkout gh-pages &&
 
 GIT_MERGE_AUTOEDIT=no git merge master &&
-
-rm -rf dist &&
-
-npm run build &&
 
 git push origin gh-pages &&
 
